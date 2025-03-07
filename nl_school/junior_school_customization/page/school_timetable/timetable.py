d6ccb6111a9d1f1e1e44cc6ca8396707e406accf
Joseph Mania <techmaniacc@Josephs-MacBook-Air.local>
2025-03-07 11:03:58 +0300

    feat - Created a new timetable with teacher and stream filters

---
+++ nl_school/junior_school_customization/page/school_timetable/timetable.py
@@ -0,0 +1,17 @@
+import frappe
+
+@frappe.whitelist()
+def get_course_schedule(instructor=None, stream=None):
+    filters = {}
+
+    if instructor:
+        filters["instructor"] = ["like", f"%{instructor}%"]
+    if stream:
+        filters["stream"] = ["like", f"%{stream}%"]
+
+    schedules = frappe.get_all(
+        "Course Schedule", 
+        filters=filters,
+        fields=["name", "course", "instructor", "student_group", "schedule_date", "from_time", "to_time","room"]
+    )
+    return schedules
