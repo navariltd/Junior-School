b878daa79563f59f5a0c520be995827ec4dbcf5b
maniamartial <martialmania19@gmail.com>
2025-04-25 17:56:17 +0300

    feat: Update the enhanced program enrollment doctype with current year, then automatically enroll students

--- nl_school/junior_school_customization/utils.py
+++ nl_school/junior_school_customization/utils.py
@@ -18,3 +18,16 @@ def create_academic_year():
         academic_year.insert(ignore_permissions=True)
         frappe.db.commit()
         frappe.msgprint(f"Created: {academic_year_name}")
+
+
+def update_enrolment_tool():
+    enrolment_doc = frappe.get_single("Enhanced Program Enrollment Tool")
+    enrolment_doc.academic_year = frappe.get_all(
+        "Academic Year",
+        filters={"year_start_date": ["<=", frappe.utils.nowdate()]},
+        fields=["name"],
+        order_by="year_start_date desc",
+        limit=1,
+    )[0].name
+    enrolment_doc.save()
+    enrolment_doc.enroll_students()
