e0241ae99f9c924b975a7923a94d44c37e900c50
maniamartial <martialmania19@gmail.com>
2025-05-12 12:56:50 +0300

    feat: Creation of new report to improve on final assessment grades

---
+++ nl_school/junior_school_customization/report/enhanced_final_assessment_grades/enhanced_final_assessment_grades.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2025, Navari and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Enhanced Final Assessment Grades"] = {
+  filters: [],
+};
