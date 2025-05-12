3b15d9a3f04e56ceb7bfe5445dccc346e7db6a2e
maniamartial <martialmania19@gmail.com>
2025-05-12 17:48:45 +0300

    feat: Add school, academic term on final assessment grade report

--- nl_school/junior_school_customization/report/enhanced_final_assessment_grades/enhanced_final_assessment_grades.js
+++ nl_school/junior_school_customization/report/enhanced_final_assessment_grades/enhanced_final_assessment_grades.js
@@ -2,5 +2,61 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Enhanced Final Assessment Grades"] = {
-  filters: [],
+  filters: [
+    {
+      fieldname: "company",
+      label: __("School"),
+      fieldtype: "Link",
+      options: "Company",
+      reqd: 1,
+    },
+
+    {
+      fieldname: "academic_year",
+      label: __("Academic Year"),
+      fieldtype: "Link",
+      options: "Academic Year",
+      reqd: 1,
+    },
+
+    {
+      fieldname: "academic_term",
+      label: __("Academic Term"),
+      fieldtype: "Link",
+      options: "Academic Term",
+      get_query: function () {
+        return {
+          filters: {
+            academic_year:
+              frappe.query_report.get_filter_value("academic_year"),
+          },
+        };
+      },
+    },
+
+    {
+      fieldname: "student_group",
+      label: __("Student Group"),
+      fieldtype: "Link",
+      options: "Student Group",
+      reqd: 1,
+      get_query: function () {
+        return {
+          filters: {
+            group_based_on: "Batch",
+            company: frappe.query_report.get_filter_value("company"),
+            academic_year:
+              frappe.query_report.get_filter_value("academic_year"),
+          },
+        };
+      },
+    },
+    {
+      fieldname: "assessment_group",
+      label: __("Assessment Group"),
+      fieldtype: "Link",
+      options: "Assessment Group",
+      reqd: 1,
+    },
+  ],
 };
---
+++ nl_school/junior_school_customization/report/utils.py
@@ -0,0 +1,69 @@
+from frappe.desk.treeview import get_children
+
+import frappe
+
+
+def get_formatted_result(args, get_course=False):
+    courses = []
+    filters = prepare_filters(args)
+
+    assessment_result = frappe.get_all(
+        "Assessment Result",
+        filters,
+        [
+            "student",
+            "student_name",
+            "name",
+            "course",
+            "assessment_group",
+            "total_score",
+            "grade",
+        ],
+        order_by="",
+    )
+
+    for result in assessment_result:
+        if get_course and result.course not in courses:
+            courses.append(result.course)
+
+        details = frappe.get_all(
+            "Assessment Result Detail",
+            {
+                "parent": result.name,
+            },
+            ["assessment_criteria", "maximum_score", "grade", "score"],
+        )
+        result.update({"details": details})
+
+    return {"assessment_result": assessment_result, "courses": courses}
+
+
+def prepare_filters(args):
+    filters = {"academic_year": args.academic_year, "docstatus": 1}
+
+    options = ["course", "academic_term", "student_group"]
+    for option in options:
+        if args.get(option):
+            filters[option] = args.get(option)
+
+    assessment_groups = get_child_assessment_groups(args.assessment_group)
+
+    filters.update({"assessment_group": ["in", assessment_groups]})
+
+    if args.students:
+        filters.update({"student": ["in", args.students]})
+    return filters
+
+
+def get_child_assessment_groups(assessment_group):
+    assessment_groups = []
+    group_type = frappe.get_value("Assessment Group", assessment_group, "is_group")
+    if group_type:
+        assessment_groups = [
+            d.get("value")
+            for d in get_children("Assessment Group", assessment_group)
+            if d.get("value") and not d.get("expandable")
+        ]
+    else:
+        assessment_groups = [assessment_group]
+    return assessment_groups
