e8abf290fd6e37818ba0c3c10b6083f194aace73
flexGURU <mukunajohn329@gmail.com>
2025-07-28 13:11:08 +0300

    feat: Dynamically populate sub_topic select field based on selected topic

--- nl_school/junior_school_customization/doctype/session/session.js
+++ nl_school/junior_school_customization/doctype/session/session.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, Navari and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Session", {
-// 	refresh(frm) {
+frappe.ui.form.on("Session", {
+  topic: function (frm) {
+    frappe.call({
+      method: "nl_school.junior_school_customization.utils.get_subtopics",
+      args: {
+        topic: frm.doc.topic,
+      },
+      callback: function (response) {
+        if (response && response.message) {
+          console.log(response);
 
-// 	},
-// });
+          const subtopics = response.message.map((item) => item.topic_name);
+          console.log(subtopics);
+
+          frm.set_df_property("sub_topic", "options", subtopics.join("\n"));
+
+          if (subtopics.length > 0) {
+            frm.set_value("sub_topic", "");
+          } else {
+            frm.set_value("sub_topic", "");
+          }
+        }
+      },
+    });
+  },
+});
