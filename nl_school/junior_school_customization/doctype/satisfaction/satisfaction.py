b89d9a4f61f159e8c872d655344e86dbafe1bc2e
flexGURU <mukunajohn329@gmail.com>
2025-07-30 18:37:16 +0300

    feat: implement average score calculation in Satisfaction document

--- nl_school/junior_school_customization/doctype/satisfaction/satisfaction.py
+++ nl_school/junior_school_customization/doctype/satisfaction/satisfaction.py
@@ -5,4 +5,18 @@ from frappe.model.document import Document
 
 
 class Satisfaction(Document):
-    pass
+    def before_save(self):
+        self.get_average()
+
+    def get_average(self):
+        scores = []
+
+        for row in self.satisfaction_scores:
+            if row.score is not None:
+                scores.append(row.score)
+
+        if scores:
+            average = sum(scores) / len(scores)
+            self.avg_score = average
+        else:
+            self.avg_score = 0
