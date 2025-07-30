35832473a2d099cbd5d0256021af27b75ccc6f2d
flexGURU <mukunajohn329@gmail.com>
2025-07-30 18:00:27 +0300

    feat: add satisfaction and score childtable

---
+++ nl_school/junior_school_customization/doctype/satisfaction/satisfaction.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Navari and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class Satisfaction(Document):
+    pass
