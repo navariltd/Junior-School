d89078255285151e42e2cc6b08ea9fce846a616d
flexGURU <mukunajohn329@gmail.com>
2025-08-04 17:32:26 +0300

    feat: refactor Parental Engagement report to improve data retrieval and update field mappings

--- nl_school/junior_school_customization/report/parental_engagement/parental_engagement.py
+++ nl_school/junior_school_customization/report/parental_engagement/parental_engagement.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2025, Navari and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.query_builder import DocType
 
 
 def execute(filters=None):
@@ -19,13 +20,13 @@ def get_columns():
             "width": 200,
         },
         {
-            "fieldname": "student",
+            "fieldname": "student_name",
             "label": "Student Name",
             "fieldtype": "Data",
             "width": 200,
         },
         {
-            "fieldname": "parent",
+            "fieldname": "parent1",
             "label": "Parent",
             "fieldtype": "Link",
             "options": "Guardian",
@@ -38,12 +39,12 @@ def get_columns():
             "width": 200,
         },
         {
-            "fieldname": "discussed_topics",
+            "fieldname": "discussed_school_topics",
             "label": "Discussed School Topics",
             "fieldtype": "Data",
         },
         {
-            "fieldname": "helped_homework",
+            "fieldname": "helped_with_homework",
             "label": "Helped with Homework",
             "fieldtype": "Data",
         },
@@ -62,5 +63,41 @@ def get_columns():
     return columns
 
 
+def map_types(filters=None):
+    if filters:
+        if filters.get("engagement_type"):
+            stripped = filters.get("engagement_type")
+            stripped.lower().replace(" ", "_")
+
+            return stripped
+
+
 def get_data(filters):
-    return []
+    PE = DocType("Parental Engagement")
+
+    query = frappe.qb.from_(PE).select(
+        PE.student,
+        PE.student_name,
+        PE.parent1,
+        PE.parent_name,
+        PE.discussed_school_topics,
+        PE.helped_with_homework,
+        PE.encouraged_education,
+        PE.frequency,
+    )
+
+    conditions = []
+    if filters.get("company"):
+        conditions.append(PE.year == filters.get("company"))
+    if filters.get("year"):
+        conditions.append(PE.year == filters.get("year"))
+    if filters.get("Engagement Type"):
+        field_name = map_types(filters)
+        if field_name:
+            conditions.append(getattr(PE, field_name) == 1)
+
+    for condition in conditions:
+        query = query.where(condition)
+    print(query.run())
+
+    return query.run()
